603e24a7fb52b397bbdd803a412a401bb2259c3f
Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>
2025-02-24 05:24:45 +0000

    Base flutter application.

---
+++ README.md
@@ -0,0 +1,3 @@
+# monster_compendium
+
+A compendium for user created 5e monsters
---
+++ android/app/src/main/kotlin/com/jayajosh/monster_compendium/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jayajosh.monster_compendium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
