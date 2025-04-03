e96f074ace852f8e57a62b5587560bf83892c85b
Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>
2025-04-03 20:44:52 +0100

    - Frame border's now theme dependant.
    - Theme notifier improved, some code redundant/to be migrated
    
    Signed-off-by: Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>

--- lib/components/photo_border.dart
+++ lib/components/photo_border.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import '../services/themes.dart';
 
 Widget photoBorder(image) {
