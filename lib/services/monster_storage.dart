5c0f24e4647ccd1729d43f469d0a03dff72df616
Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>
2025-04-08 17:50:44 +0100

    - pb calc added on upload - xp calc added on upload
    Signed-off-by: Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>

--- lib/services/monster_storage.dart
+++ lib/services/monster_storage.dart
@@ -166,8 +166,8 @@ class MonsterStore {
       'type': type,
       'alignment': alignment,
       'cr': cr,
-      'xp': 1000, //todo add this to data upload
-      'proficiency_bonus': 1, //todo this too
+      'xp': calcXP(cr),
+      'proficiency_bonus': proficiencyBonus(this.cr!),
       'ac': ac,
       'hit_points': hit_points,
       'hit_dice': hit_dice,
@@ -193,9 +193,53 @@ class MonsterStore {
 
   upload(){
     final db = FirebaseFirestore.instance;
-    print('uploading');
     db.collection('Monsters').doc().set(toMap())
         .onError((e, _) => print("Error writing document: $e")); //todo snackbar notify
 
   }
+
+    int proficiencyBonus(double cr) {
+      if (cr == 0) return 2;
+      return ((cr - 1) / 4).floor() + 2;
+    }
+}
+
+calcXP(cr){
+   final Map<double, int> crToXp = {
+    0: 10,
+    0.125: 25,
+    0.25: 50,
+    0.5: 100,
+    1: 200,
+    2: 450,
+    3: 700,
+    4: 1100,
+    5: 1800,
+    6: 2300,
+    7: 2900,
+    8: 3900,
+    9: 5000,
+    10: 5900,
+    11: 7200,
+    12: 8400,
+    13: 10000,
+    14: 11500,
+    15: 13000,
+    16: 15000,
+    17: 18000,
+    18: 20000,
+    19: 22000,
+    20: 25000,
+    21: 33000,
+    22: 41000,
+    23: 50000,
+    24: 62000,
+    25: 75000,
+    26: 90000,
+    27: 105000,
+    28: 120000,
+    29: 135000,
+    30: 155000,
+  };
+   return crToXp[cr];
 }
