6d4e578cd24e0b5d063b25edb6447f3847e353c0
Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>
2025-04-17 19:31:51 +0100

    - user factory built
    Signed-off-by: Joshua Aston-Adams <52387076+jayajosh@users.noreply.github.com>

---
+++ lib/services/user_factory.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:monster_compendium/services/photo_service.dart';
+
+class UserStore {
+  String uid = FirebaseAuth.instance.currentUser!.uid;
+  String? username;
+  String? bio;
+  String? image_url;
+  List<String> saved_monsters = [];
+  List<String> liked_monsters = [];
+
+
+
+  UserStore({
+    this.username,
+    this.bio,
+    this.image_url,
+    required this.saved_monsters,
+    required this.liked_monsters,
+
+  });
+
+  factory UserStore.fromMap(Map<String, dynamic> map) {
+    return UserStore(
+      username: map['name'],
+      bio: map['size'],
+      image_url: map['type'],
+      saved_monsters: map['alignment'],
+      liked_monsters: map['cr'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    //Converts to a map for firestore upload
+    return {
+      'username': username,
+      'bio': bio,
+      'image_url': image_url,
+      'saved_monsters': saved_monsters,
+      'liked_monsters': liked_monsters,
+    };
+  }
+
+  create(context,userMap) async {
+    final db = FirebaseFirestore.instance;
+    //image_url = await storeChild(uid,image,context); //todo decide whether to wait or not
+    db.collection('Monsters').doc(uid).set(userMap)
+        .onError((e, _) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Error writing document: $e"),
+      duration: const Duration(seconds: 5),
+    )));
+
+  }
+
+  update(context,Map monsterMap) async {
+    final db = FirebaseFirestore.instance;
+    //image_url = await storeChild(uid,image,context); //todo decide whether to wait or not
+    var uploadMonsterMap = monsterMap.cast<Object,Object>();
+    db.collection('Users').doc(uid).update(uploadMonsterMap)
+        .onError((e, _) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Error writing document: $e"),
+      duration: const Duration(seconds: 5),
+    )));
+
+  }
+
+    validate(context){
+      Map userMap = toMap();
+      final validation = ['username','bio'];
+      String missing = '';
+      for (var field in validation) {
+        if(userMap[field] == null){
+          missing += '$field, ';
+      }
+      }
+      if(missing == '') {
+        update(context, userMap);
+      }
+      else{
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Missing fields: ${missing.substring(0,missing.length-2)}")));}
+    }
+}
